3282927c5137a0e43103bc939c299a7f93ff817b
Loianne Mendes <loianne@outlook.com.br>
2024-11-22 00:52:54 -0300

    deletes

--- cadastro.js
+++
@@ -1,6 +0,0 @@
-const formCadastro = document.getElementById('formCadastro');
-formCadastro.addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Cadastro realizado com sucesso!');
-    window.location.href = './index.html'
-});
